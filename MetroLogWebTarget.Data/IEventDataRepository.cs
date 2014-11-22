6c627110013d4e89ea75ef61c0cba1f71cbc36e0
hystar <anonymouse@me.com>
2014-11-22 21:41:49 +0800

    项目结构变化

---
+++ MetroLogWebTarget.Data/IEventDataRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using AnimalMarketDal.DomainModel;
+
+namespace AnimalMarketDal.Repo
+{
+    public interface IEventDataRepository : IDisposable
+    {
+        AnimalType GetCowType();
+
+        AnimalType GetPigType();
+
+        AnimalType GetLambType();
+
+        IEnumerable<EventData> GetPigData();
+
+        IEnumerable<EventData> GetLambData();
+
+        IEnumerable<EventData> GetCalfData();
+    }
+}
