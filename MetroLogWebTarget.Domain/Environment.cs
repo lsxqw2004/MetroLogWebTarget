bf17f3dc27b85f17dfa8480a785db63a0b1d1b8c
hystar <anonymouse@me.com>
2014-11-22 21:54:16 +0800

    总有些东西一次提交不了呢

---
+++ MetroLogWebTarget.Domain/Environment.cs
@@ -0,0 +1,23 @@
+﻿namespace MetroLogWebTarget.Domain
+{
+    public class Environment
+    {
+
+        public int Id { get; set; }
+
+        public string PackageArchitecture { get; set; }
+
+        public string PackageFullName { get; set; }
+
+        public string PackagePublisher { get; set; }
+
+        public string PackagePublisherId { get; set; }
+
+        public string PackageResourceId { get; set; }
+
+        public string PackageVersion { get; set; }
+
+        public string InstallationId { get; set; }
+
+    }
+}
---
+++ MetroLogWebTarget.Domain/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroLogWebTarget.Domain
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public int BatId { get; set; }
+
+        public int SequenceId { get; set; }
+
+
+        public string Logger { get; set; }
+
+
+        public string Level { get; set; }
+
+        public string Message { get; set; }
+
+        public string Exception { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+    }
+}
